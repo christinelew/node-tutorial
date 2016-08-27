69e1c64ee1acd808f2581cc0ea5499ca725136a2
matt-winn <matt.winn@parivedasolutions.com>
2016-08-27 12:01:25 -0700

    No more . referencing!

--- index.js
+++ index.js
@@ -138,9 +138,9 @@ function mail(response, request) {
                         response.end();
                     }
                     else if (result) {
-                        console.log('Result: ' + JSON.stringify(result));
+                        // console.log('Result: ' + JSON.stringify(result));
                         console.log('getMessages returned ' + result.value.length + ' messages.');
-                        // response.write('<p>Total Number of Unread Emails ' + result.count + ' messages</p>');
+                        response.write('<p>Total Number of Unread Emails ' + result['@odata.count'] + ' messages</p>');
                         response.write('<table><tr><th>From</th><th>Subject</th><th>Received</th></tr>');
                         result.value.forEach(function (message) {
                             console.log('  Subject: ' + message.Subject);
